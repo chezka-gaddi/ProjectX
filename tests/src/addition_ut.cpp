c1a1a8e02ef0044c2d1863bd5bc96ded7c9a5f89
Riley Kopp <riley.kopp@mines.sdsmt.edu>
2018-08-24 08:43:01 -0600

    Added Comments and stuff to the UT framework and example

--- tests/src/addition_ut.cpp
+++ tests/src/addition_ut.cpp
@@ -1,6 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////
+// File name: addition_ut.cpp
+// Author: Riley Kopp
+// Date Updated: 24 Aug 18
+// Description: Example UT file
+////////////////////////////////////////////////////////////////////////////////
+
+// Includes
 #include "catch.hpp"
 #include "../../src/addition.h"
 
+////////////////////////////////////////////////////////////////////////////////
+// Scenario: Addition Adds two numbers
+// Given:   An addition class and two numbers
+// When :   The two numbers are passed into addition
+// Then :   The returned value is the sum of the two numbers
+////////////////////////////////////////////////////////////////////////////////
 TEST_CASE("Adds Two numbers")
 {
    int x = 5;
@@ -11,10 +25,16 @@ TEST_CASE("Adds Two numbers")
    REQUIRE(add.two_values(x, y) == 55);
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// Scenario: Addition Adds two negative numbers
+// Given:   An addition class and two negative numbers
+// When :   The two numbers are passed into addition
+// Then :   The returned value is the sum of the two numbers
+////////////////////////////////////////////////////////////////////////////////
 TEST_CASE("Adds Two Negatives")
 {
    int x = -1;
-   int y = -5;
+   int y = 5;
 
    Addition add;
 
