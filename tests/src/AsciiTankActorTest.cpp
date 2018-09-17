d9a27f4bd8e1a4ad2e7242b2cbe72dee22a21362
Hinker, Paul <youki.lewis@mines.sdsmt.edu>
2018-09-17 12:15:51 -0600

    Updating tests

---
+++ tests/src/AsciiTankActorTest.cpp
@@ -0,0 +1,11 @@
+#include "catch.hpp"
+#include "../../src/AsciiTankActor.h"
+
+using namespace std;
+
+TEST_CASE("Call move function to create and return MoveData struct")
+{
+   AsciiTankActor tank;
+   tank.move( 1, 9);
+   
+}
