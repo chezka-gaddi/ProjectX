a65bc7947bab79c66bed3bec7edaba49285f29bb
Weatherhead, John <john.weatherhead@mines.sdsmt.edu>
2018-09-18 16:08:58 -0600

    Added a second constructor and unit tests for the new constuctor.
    This constructor will allow functions to initialize member variable within Actor to a specified value

--- tests/src/ActorTesting.cpp
+++ tests/src/ActorTesting.cpp
@@ -1,7 +1,7 @@
-#define testing
 #include "catch.hpp"
 #include "../../src/Actor.h"
 #include <string>
+#include <iostream>
 
 // Testing setters and default constructor
 TEST_CASE("Calling getName() member function and checking construstor")
@@ -13,6 +13,7 @@ TEST_CASE("Calling getName() member function and checking construstor")
 
 TEST_CASE("Calling getId() member function and checking construstor")
 {
+     std::cout << "Made it here" << endl;
      Actor actor;
      int test = actor.getId();
      REQUIRE(test == 0);
@@ -22,28 +23,43 @@ TEST_CASE("Calling getFieldX() member function and checking construstor")
 {
      Actor actor;
      int test = actor.getFieldX();
-     REQUIRE(test == 0);
+     REQUIRE(test == -1);
 }
 
 TEST_CASE("Calling getFieldY() member function and checking construstor")
 {
      Actor actor;
      int test = actor.getFieldY();
-     REQUIRE(test == 0);
+     REQUIRE(test == -1);
 }
 
 TEST_CASE("Calling getHealth() member function and checking construstor")
 {
      Actor actor;
      int test = actor.getHealth();
-     REQUIRE(test == 0);
+     REQUIRE(test == -1);
 }
 
 TEST_CASE("Calling getDamage() member function and checking construstor")
 {
      Actor actor;
      int test = actor.getDamage();
-     REQUIRE(test == 0);
+     REQUIRE(test == 1);
+}
+
+// Testing constructor with given values
+TEST_CASE("Test Constructor the will inialize all member variables from input variables")
+{
+     string nameTest = "DEFAULT";
+     int testArry[5] = { 67 , 98 , 34 , 21 , 93 };
+     Actor actor("DEFAULT", 67, 98, 34, 21, 93);
+
+     REQUIRE(nameTest == actor.getName());
+     REQUIRE(testArry[0] == actor.getId()); 
+     REQUIRE(testArry[1] == actor.getFieldX());
+     REQUIRE(testArry[2] == actor.getFieldY());
+     REQUIRE(testArry[3] == actor.getHealth());
+     REQUIRE(testArry[4] == actor.getDamage());
 }
 
 // Testing setters
