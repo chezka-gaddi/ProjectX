c5fd430ca8c46b2410a6e687874ca88e242398a0
Peterson, Bradley <bradley.peterson@mines.sdsmt.edu>
2018-09-20 14:47:43 -0600

    Added tests and Getters/Setters

--- src/ProjectileActor.cpp
+++ src/ProjectileActor.cpp
@@ -6,33 +6,46 @@ using namespace std;
 ProjectileActor()
 {
 	range = 10;
-	startX = field_x;
-	startY = field_y;
-	endX = startX;
-	endY = startY;
+	startX = 0;
+	startY = 0;
+	endX = 0;
+	endY = 0;
 }
 
 MoveData ProjectileActor::move()
 {
-	static int moveSpeedX = 1;
-	static int moveSpeedY = 1;
+	int xDiff, yDiff; // variables used to for movement in each direction
 	//struct for projectile's coordinates
 	moveData moveProjectile;
 	
-	//initialize data
-	moveProjectile.new_x = startX;
-	moveProjectile.new_y = staryY;
-	
-	//move projectile
-	endX = startX + moveSpeedX;
-	endY = startY + moveSpeedY;
-	
-	moveProjectile.new_x = endX;
-	moveProjectile.new_y = endY;
-	
-	//update new start position of projectile
-	startX = endX;
-	startY = endY;
+	//vertical movement
+   	 if (startX == endX)
+    	{
+       	 moveProjectile.new_x = startX;
+       	 moveProjectile.new_y = startY + range;
+    	}
+    	//horizontal movement
+    	else if(startY == endY)
+    	{
+       	 moveProjectile.new_x = startX + range;
+       	 moveProjectile.new_y = startY;
+    	}
+    	//diagonal cases
+    	else
+    	{
+         xDiff = endX - startX;
+         yDiff = endY - startY;
+
+        //scaling movement to be less than maximum range
+        if(xDiff + yDiff > range)
+        {
+            xDiff = float(xDiff / (xDiff + yDiff)) * range;
+            yDiff = float(yDiff / (xDiff + yDiff)) * range;
+        }
+
+       	 moveProjectile.new_x = startX + xDiff;
+       	 moveProjectile.new_y = startY +yDiff;
+   	}
 	
 	return moveProjectile;
 }
@@ -47,5 +60,55 @@ AttackData attack()
 	attackProjectile.damage = 1;
 	
 	return attackProjectile;
+}
+
+
+//Getters
+int ProjectileActor::getRange()
+{
+    return range;
+}
+int ProjectileActor::getStartX()
+{
+    return startX;
+}
+int ProjectileActor::getStartY()
+{
+    return startY;
+}
+int ProjectileActor::getEndX()
+{
+    return endX;
+}
+int ProjectileActor::getEndY()
+{
+    return endY;
+}
+
+
+//Setters
+void ProjectileActor::setRange(int rangeUpdate)
+{
+    range = rangeUpdate;
+}
+
+void ProjectileActor::setStartX(int startXUpdate)
+{
+    startX = startXUpdate;
+}
+
+void ProjectileActor::setStartY(int startYUpdate)
+{
+    startY = startYUpdate;
+}
+
+void ProjectileActor::setEndX(int endXUpdate)
+{
+    endX = endXUpdate;
+}
+
+void ProjectileActor::setEndY(int endYUpdate)
+{
+    endY = endYUpdate;
 }
 
--- src/ProjectileActor.h
+++ src/ProjectileActor.h
@@ -6,6 +6,7 @@
 #define SLACKERS_PLATFORM_PROJECTILEACTOR_H
 
 #include "Actor.h"
+#include <cmath>
 class ProjectileActor:public Actor{
 protected:
     int range;
@@ -19,6 +20,19 @@ public:
     MoveData move() override;
 
     AttackData attack() override;
+
+    //Getters
+int ProjectileActor::getRange();
+int ProjectileActor::getStartX();
+int ProjectileActor::getStartY();
+int ProjectileActor::getEndX();
+int ProjectileActor::getEndY();
+//Setters
+void ProjectileActor::setRange(int rangeUpdate);
+void ProjectileActor::setStartX(int startXUpdate);
+void ProjectileActor::setStartY(int startYUpdate);
+void ProjectileActor::setEndX(int endXUpdate);
+void ProjectileActor::setEndY(int endYUpdate);
 };
 
 
