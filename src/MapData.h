3b05a6fb16cc975848af32e374e20b684715be14
David Donahue <david.donahue@mines.sdsmt.edu>
2018-09-23 17:02:36 -0600

    added MapData.h, which contains a struct with the map and dimensions of the field for easier passing.

---
+++ src/MapData.h
@@ -0,0 +1,12 @@
+/**
+ * MapData.h
+ * struct that contains the map and the dimensions of the field.
+ */
+#include <vector>
+
+struct MapData
+{
+    //row-major-order list the ID's of anything on the tiles on the map (tanks, projectiles, and obstacles)
+    std::vector<int> map;
+    int width, height;
+}
