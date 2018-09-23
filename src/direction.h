520893e317fdf60aee0c925fe632948175c85273
David Donahue <david.donahue@mines.sdsmt.edu>
2018-09-23 17:58:39 -0600

    Added ifndef...endif statements to direction.h and MapData.h

--- src/direction.h
+++ src/direction.h
@@ -2,5 +2,9 @@
  * File: directions.h
  * This file contains an enum of possible move directions, to be used for the move() and attack() fucntions of Actor derrivatives.
  */
+#ifndef __DIRECTION_H
+#define __DIRECTION_H
 
 enum direction {left, right, up, down, stay};
+
+#endif
