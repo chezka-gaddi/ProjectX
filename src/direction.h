a54bf8393f7a85de932675b75aeeccd59971e158
David Donahue <david.donahue@mines.sdsmt.edu>
2018-09-23 17:00:12 -0600

    changed name of direction.h to reflect the enum name

---
+++ src/direction.h
@@ -0,0 +1,6 @@
+/**
+ * File: directions.h
+ * This file contains an enum of possible move directions, to be used for the move() and attack() fucntions of Actor derrivatives.
+ */
+
+enum direction {left, right, up, down, stay};
