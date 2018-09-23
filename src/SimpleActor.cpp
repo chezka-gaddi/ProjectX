73f23d763b235607323a4fc98dc2e2b0530a6021
David Donahue <david.donahue@mines.sdsmt.edu>
2018-09-23 17:53:20 -0600

    Added Actor-Alternate class and SimpleActor derrived class to do my testing as the verions of Actor on the repo do not reflect the planned interface. Actor-Alternate will be obsolete once the new version of Actor.h is pushed up by John.

---
+++ src/SimpleActor.cpp
@@ -0,0 +1,11 @@
+#include "SimpleActor.h"
+
+direction SimpleActor::move(MapData map, PositionData status)
+{
+    return stay;
+}
+AttackData SimpleActor::attack(MapData map, PositionData status)
+{
+    AttackData ret;
+    return ret;
+}
--- src/directions.h
+++
@@ -1,6 +0,0 @@
-/**
- * File: directions.h
- * This file contains an enum of possible move directions, to be used for the move() and attack() fucntions of Actor derrivatives.
- */
-
-enum direction {left, right, up, down, stay};
