5e81f36fe91313914d05e016aed699552e7e32f0
Jacob Lee <jacob.lee@mines.sdsmt.edu>
2018-09-24 02:58:15 -0600

    added comments and doxygen

--- src/main.cpp
+++ src/main.cpp
@@ -21,14 +21,16 @@
 
 int main(void)
 {
+
+   //this is the start up of the game logic atleast 2 tanks need to be on the field at any given time
    GameField gameField = GameField(10,10);
    //change tankactor here to what ever we have decided to call the ascii tank actor
    TankActor * player1 = new TankActor();
    TankActor * player2 = new TankActor();
-
+   //tank actor pointers are made and then packaged into ActorInfo structs
    ActorInfo player1Info = ActorInfo();
    ActorInfo player2Info = ActorInfo();
-
+   //assigning data values to the actor info made it verbose to tell what exactly is happening
    player1Info.act_p = player1;
    player2Info.act_p = player2;
 
@@ -45,18 +47,18 @@ int main(void)
 
    player1Info.id = 2;
    player2Info.id = 2;
-
+   //adding the TankActors to the game field
    gameField.addActor(player1Info);
    gameField.addActor(player2Info);
 
-
+   //the is the main game loop
    while(isplayable(gameField.getActors())){
-
+      //displays the current game state
       std::vector<ActorInfo> actors = gameField.getActors();
       displayAscii(gameField, actors);
-
+      //makes the next moves
       gameField.nextTurn();
-
+      //removes all actors with health == 0;
       gameField.cull();
    }
 
