7e1bd18bf312adf553b67dafbf18f57329fcc5a0
David Donahue <david.donahue@mines.sdsmt.edu>
2018-09-23 21:41:10 -0600

    added == overload for ActorInfo, confirmed findActorsByCoord working

--- tests/src/GameFeildTests.cpp
+++ tests/src/GameFeildTests.cpp
@@ -37,27 +37,28 @@ TEST_CASE("findActorByCoord() works with no actors")
     GameField g;
     REQUIRE(g.findActorsByCoord(0,0).size() == 0);
 }
-/*
+
 TEST_CASE("findActorByCoord() returns correct actors")
 {
     GameField g;
-    Actor a1("", 0, 0, 0, 0, 0);
-    Actor a2("", 0, 0, 0, 0, 0);
-    Actor a3("", 0, 1, 1, 0, 0);
-    std::vector<Actor *> hits;
+    ActorInfo a1 (NULL, 1, 1, 0, 0, 1);
+    ActorInfo a2 (NULL, 2, 1, 0, 0, 1);
+    ActorInfo a3 (NULL, 3, 1, 1, 1, 1);
     
-    g.addActor(&a1);
-    g.addActor(&a2);
-    g.addActor(&a3);
+    
+    
+    g.addActor(a1);
+    g.addActor(a2);
+    g.addActor(a3);
 
-    hits = g.findActorsByCoord(0,0);
+    std::vector<ActorInfo> hits = g.findActorsByCoord(0,0);
     //make sure that 2 actors are returned and they are the right ones
     REQUIRE((hits.size() == 2
-             && (hits[0] == &a1 || hits[1] == &a1)
-             && (hits[0] == &a2 || hits[1] == &a2) ));
+             && (hits[0] == a1 || hits[1] == a1)
+             && (hits[0] == a2 || hits[1] == a2) ));
     
 }
-
+/*
 TEST_CASE("cull() removes only actors with health of 0")
 {
     GameField g;
