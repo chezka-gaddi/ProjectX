5a44b070a1bf9c474dc42e183e53fd69409bba6b
David Donahue <david.donahue@mines.sdsmt.edu>
2018-09-23 18:06:29 -0600

    implemented SimpleActor.attack()

--- tests/src/SimpleActorTests.cpp
+++ tests/src/SimpleActorTests.cpp
@@ -15,3 +15,12 @@ TEST_CASE("SimpleActor.move() returns up")
     PositionData p;
     REQUIRE(a.move(m,p) == up);
 }
+TEST_CASE("SimpleActor.attack() attacks 0,0")
+{
+    SimpleActor a;
+    MapData m;
+    PositionData p;
+    AttackData result;
+    result = a.attack(m,p);
+    REQUIRE(( result.attack_x == 0 && result.attack_y == 0));
+}
