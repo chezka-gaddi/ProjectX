9564e5b66df29d053657b2c0e6dc49b8d6e1b210
David Donahue <david.donahue@mines.sdsmt.edu>
2018-10-04 18:33:46 -0600

    Added test to ensure the constructor for SimpleActor is working and added SimpleActorTests back into the Makefile

--- tests/src/SimpleActorTests.cpp
+++ tests/src/SimpleActorTests.cpp
@@ -1,6 +1,6 @@
 #include "catch.hpp"
 #include <SimpleActor.h>
-#include <Actor-Alternate.h>
+#include <Actor.h>
 
 
 TEST_CASE("SimpleActor constructs correctly")
@@ -8,7 +8,7 @@ TEST_CASE("SimpleActor constructs correctly")
     SimpleActor a;
     REQUIRE(sizeof(a) > 0);
 }
-TEST_CASE("SimpleActor.move() returns up")
+TEST_CASE("default SimpleActor.move() returns up")
 {
     SimpleActor* a = new SimpleActor; //Apparently the constructor does not run when delcaring directly.
     MapData m;
@@ -16,7 +16,7 @@ TEST_CASE("SimpleActor.move() returns up")
     REQUIRE(a->move(m,p) == up);
     delete a;
 }
-TEST_CASE("SimpleActor.attack() attacks 0,0")
+TEST_CASE("default SimpleActor.attack() attacks 0,0 with 0 damage")
 {
     SimpleActor* a = new SimpleActor;
     MapData m;
@@ -24,5 +24,13 @@ TEST_CASE("SimpleActor.attack() attacks 0,0")
     AttackData result;
     result = a->attack(m,p);
     REQUIRE(( result.attack_x == 0 && result.attack_y == 0 && result.damage == 0));
+    delete a;
+}
+TEST_CASE("SimpleActor moves in the direction specified at construction")
+{
+    SimpleActor* a = new SimpleActor(down, 0);
+    MapData m;
+    PositionData p;
+    REQUIRE(a->move(m,p) == down);
     delete a;
 }
