358f8dc0f9d61841a93bd9df32a7739d9c9aae40
Youki <youki.lewis@mines.sdsmt.edu>
2018-10-05 11:11:39 -0600

    Added numbers around the grid, all tests are still passing

--- src/MapData.cpp
+++ src/MapData.cpp
@@ -7,62 +7,72 @@ std::ostream& operator<<(std::ostream& os, const MapData& md)
     int i, j;
     //ANSI Stuff
     os << "\x1B[2J"; // clear screen and home cursor
+    os << " ";
+    for	(j = 0; j < md.width; ++j)
+    {
+	if ( j < 10 )
+	    os << "   " << j << "   ";
+	else 
+	    os << "   " << j << "  ";
+    }
+    os << '\n';
+    os << ' ';
     for (j = 0; j < md.width; ++j)
     {
-        os << "______";
+        os << " ______";
     }
     os << '\n';
     for (i = 0; i < md.height; ++i)
     {
-        os << '|';
+        os << " |";
         for (j = 0; j < md.width; ++j) //top of cell
         {
-            os << "     |";
+            os << "      |";
         }
-        os <<"\n|";
+        os <<"\n |";
         for (j = 0; j < md.width; ++j) // top of the tank 'sprite'
         {
             if (md.map[j+i*md.width] > 0) //tank actor
             {
-                os << " o-o |";
+                os << "  o-o |";
             }
             else
             {
-                os << "     |";
+                os << "      |";
             }
         }
-        os <<"\n|";
+        os <<"\n" << i <<"|";
         for (j = 0; j < md.width; ++j) //middle of cell
         {
             if (md.map[j+i*md.width] > 0) //tank actor
             {
-                os << " |" << md.map[j+i*md.width] << "| |";
+                os << "  |" << md.map[j+i*md.width] << "| |";
             }
             else if (md.map[j+i*md.width] < 0) // projectile actor
             {
-                os << "  *  |";
+                os << "   *  |";
             }
             else
             {
-                os << "     |";
+                os << "      |";
             }
         }
-        os <<"\n|";
+        os <<"\n" << " |";
         for (j = 0; j < md.width; ++j) //bottom of tank 'sprite
         {
             if (md.map[j+i*md.width] > 0) //tank actor
             {
-                os << " o-o |";
+                os << "  o-o |";
             }
             else
             {
-                os << "     |";
+                os << "      |";
             }
         }
-        os <<"\n|";
+        os <<"\n |";
         for (j = 0; j < md.width; ++j) //bottom of cell
         {
-            os << "_____|";
+            os << "______|";
         }
         os << '\n'; //end of row
     }
