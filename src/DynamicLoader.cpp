066322175cb87b3b5e338002802b55fac4c950be
David Donahue <david.donahue@mines.sdsmt.edu>
2018-10-13 15:02:10 -0600

    Added comments

--- src/DynamicLoader.cpp
+++ src/DynamicLoader.cpp
@@ -29,11 +29,12 @@ std::vector<Actor *> dynamicTankLoader(std::vector<std::string> objectNames)
     std::string symName;
     for (auto s : objectNames)
     {
+        //build up the full realative file path from the given name
         soPath = (relPath);
         soPath.append(s);
         soPath.append(".so");
 
-        
+        //get a 'handle' to the .so file, which can be used to load symbols
         void * handle = dlopen(soPath.c_str(), RTLD_LAZY); //open the .so
         if (handle == NULL) //make sure it opened
         {
@@ -43,13 +44,13 @@ std::vector<Actor *> dynamicTankLoader(std::vector<std::string> objectNames)
         else
         {
             Actor* (*maker)() = (Actor* (*)()) dlsym(handle, "maker"); //get the maker() symbol
-            if (maker == NULL)
+            if (maker == NULL) //If the symbol was not found in the .so
             {
                 std::cout << "Failed to load symbol " << symName << '\n';
             }
             else
             {
-                ret.push_back(maker());
+                ret.push_back(maker()); //Run maker() to construct an instance of the class
             }
         }
 
