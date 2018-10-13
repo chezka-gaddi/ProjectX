ede3c7757990e4e1c477d66515db6bf8de5364b6
David Donahue <david.donahue@mines.sdsmt.edu>
2018-10-13 13:31:00 -0600

    Made the symbol lookup **HOPEFULLY** able to work with any tank

--- src/DynamicLoader.cpp
+++ src/DynamicLoader.cpp
@@ -15,26 +15,37 @@ std::vector<Actor *> dynamicTankLoader(std::vector<std::string> objectNames)
 {
     std::vector<Actor *> ret;
     std::string relPath ("./tanks/");
+    std::string soPath;
+    std::string symName;
     for (auto s : objectNames)
     {
-        s.insert(0, relPath);
-        s.append(".so");
-        void * handle = dlopen(s.c_str(), RTLD_LAZY);
-        if (handle == NULL)
+        soPath = (relPath);
+        soPath.append(s);
+        soPath.append(".so");
+
+        //C++ is nice enough to scramble all the symbol names, and there is no way to get them at runtime...
+        //Fun! this should predict the scrambled name based on my testing, but it is likely extraordinarily
+        //fragile. TODO: find a way to read the symbol list into the program and search for maker
+        symName = "_ZN8";
+        symName.append(s);
+        symName.append("5makerEv");
+            
+        
+        void * handle = dlopen(soPath.c_str(), RTLD_LAZY); //open the .so
+        if (handle == NULL) //make sure it opened
         {
             std::cout << "Failed to open " << s << "\n";
             std::cout << dlerror() << std::endl;
         }
         else
         {
-            Actor* (*maker)() = (Actor* (*)()) dlsym(handle, "_ZN8SimpleAI5makerEv");
+            Actor* (*maker)() = (Actor* (*)()) dlsym(handle, symName.c_str()); //get the maker() symbol
             if (maker == NULL)
             {
-                std::cout << "Failed to load symbol\n";
+                std::cout << "Failed to load symbol " << symName << '\n';
             }
             else
             {
-                std::cout << maker() << std::endl;
                 ret.push_back(maker());
             }
         }
