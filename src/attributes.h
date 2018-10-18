4dba1a587cfeac407128e2af9c40755c29a12358
Riley Kopp <riley.kopp@mines.sdsmt.edu>
2018-10-18 12:47:17 -0600

    modifed the attributes struct to support radar

--- src/attributes.h
+++ src/attributes.h
@@ -7,9 +7,10 @@ struct attributes
     int tankDamage;
     int tankRange;
     int tankShots;
+    int tankRadar;
 
-    attributes( int tH, int tD, int tR, int tS ) : tankHealth(tH), 
-    tankDamage(tD), tankRange(tR), tankShots(tS) {}
+    attributes( int tH, int tD, int tR, int tS, int tV ) : tankHealth(tH), 
+    tankDamage(tD), tankRange(tR), tankShots(tS), tankRadar(tV) {}
     
     attributes(){}
 
