c7428c361fe9cdacd9dbe4d958b976c7edefe3a3
Riley Kopp <riley.kopp@mines.sdsmt.edu>
2018-10-31 11:47:06 -0600

    added some stuff

--- helper/Bracket_1v1/build_bracket.cpp
+++ helper/Bracket_1v1/build_bracket.cpp
@@ -11,116 +11,90 @@
 
 using namespace std;
 
+// Input looks like ./EXE tank_list map_list
+ 
 int main (int argc, char ** argv)
 {
     ifstream tank_list;
     ofstream game_config;
     ifstream map;
-    ifstream map_list_file;
-
+    ifstream map_list;
+    
     string temp;
-
-    if(argc != 3)
+    vector<string> tanks;
+    vector<string> maps;
+    string base_file_name;
+    int map_id = 0;
+    // Load Tanks
+    tank_list.open(argv[1]);
+    if (!tank_list.is_open())
     {
-        cout << "Not the right amount of arguments"
-             << endl
-             << "Command Should Look Like:"
-             << endl
-             << "./generate_bracket tank_list map_list"
-             << endl;
+        cout << "Cant open tanks" << endl;
         exit(1);
     }
 
-
-    vector<string> map_list;
-    vector<string> round_matchup;
-    map_list_file.open(argv[2]);
-
-    while(!map_list_file.eof())
-    {
-        getline(map_list_file, temp);
-        map_list.push_back(temp);
-    }
-    map_list_file.close();
-
-    string tank1;
-    string tank2;
-    string config_name = "OneVOneTournament/Round_1_1";
-
-    int random;
-
-    tank_list.open(argv[1], ios::in);
-
-
-
     while(!tank_list.eof())
     {
-        getline(tank_list, tank1);
-        getline(tank_list, tank2);
+        getline(tank_list, temp);
+        tanks.push_back(temp);
+    }
+    tank_list.close();
 
-        round_matchup.push_back(tank1);
-        round_matchup.push_back(tank2);
 
-        if(tank1.empty() || tank2.empty())
+    // Load Maps
+    map_list.open(argv[2]);
+    if (!map_list.is_open())
+    {
+        cout << "Cant open maps" << endl;
+        exit(1);
+    }
+
+    while(!map_list.eof())
+    {
+        getline(map_list, temp);
+        maps.push_back(temp);
+    }
+
+    map_list.close();
+
+    // Build a Game for each possible tank combo
+    
+    
+    srand(time(NULL));
+    for(int tank_1 = 0; tank_1 < tanks.size() - 1; tank_1++)
+    {
+        for(int tank_2 = tank_1 + 1; tank_2 < tanks.size() - 1 ; tank_2++)
         {
-            break;
-        }
-
-        // TODO Check for odd number of tanks
-
-        game_config.open(config_name.c_str());
-
-        random = rand() % map_list.size() - 1;
-        game_config << "#Map: "<< map_list[random] << endl;
-        map.open(map_list[random]);
-
-        if(map.is_open())
-        {
-            cout << "Map Opened" << endl;
-            while(temp != "Spawn Points x, y")
+        base_file_name = tanks.at(tank_1) + "vs" + tanks.at(tank_2);
+        game_config.open(base_file_name);
+            map_id = rand() % maps.size() - 1;
+            
+            map.open(maps[map_id]);
+            // copy over the map to the game config
+            game_config << "Map: " << maps.at(map_id) << endl;
+            while(!map.eof())
             {
                 getline(map, temp);
                 game_config << temp << endl;
             }
-
-            game_config << "# Tank List" << endl;
-            while( round_matchup.size() != 0)
-            {
-               getline(map, temp);
-               game_config << "AI "  <<round_matchup.back() << " " << temp << endl;
-               round_matchup.pop_back();
-            }
+            map.close();
+            
+            game_config << "AI " << tanks.at(tank_1) << " 0 0 images/Blue/tankB_U.png images/Blue/tankB_R.png images/Blue/tankB_D.png images/Blue/tankB_L.png images/Blue/bulletB.png" << endl;
+            game_config << "AI " << tanks.at(tank_2) << " 29 12 images/Red/tankR_U.png images/Red/tankR_R.png images/Red/tankR_D.png images/Red/tankR_L.png images/Red/bulletR.png" << endl;
 
 
-        }
-        map.close();
+            game_config << "#Game Rules" << endl;
 
-        tank1.erase();
-        tank2.erase();
-
-
-
-
-        game_config  << endl << "#Tank rules" << endl
-            << "DAMAGE 1"
-            << endl
-            << "HEALTH 2"
-            << endl
-            << "AP 2"
-            << endl
-            << "SPECIAL 1"
-            << endl;
-
-
-
+            game_config << "DAMAGE 1" << endl;
+            game_config << "HEALTH 2" << endl;
+            game_config << "RADAR 10" << endl;
+            game_config << "AP 5" << endl;
+            game_config << "SPECIAL 1" << endl;
 
         game_config.close();
-        config_name.back()++;
-
-
+        }
     }
 
-    tank_list.close();
 
     return 0;
 
