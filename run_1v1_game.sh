9bf990b15305a8b5d5baca5a10e114277f571483
Riley Kopp <riley.kopp@mines.sdsmt.edu>
2018-11-02 09:52:32 -0600

    added an output for the tournament

--- run_1v1_game.sh
+++ run_1v1_game.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo tournament_1v1/*.round > temp.txt
+rm outcomes
 
 cat temp.txt | sed "s/ /\n/g" > temp_2.txt
 rm temp.txt
@@ -12,7 +13,7 @@ until [ "$actual_size" -eq "$empty_size" ]; do
     TEXT=$(head -n 1 temp_2.txt)
 #    echo $actual_size
     cp $TEXT ./config.txt
-    ./platform
+    ./platform >> outcomes
     tail -n +2 temp_2.txt > temp_2.txt
     actual_size=$(wc -c < "temp_2.txt")
 done
