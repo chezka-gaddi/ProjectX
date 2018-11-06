dc4f47a53e7ea65345cad7c28e95e37be75e273b
jamckee <dj32mckee@hotmail.com>
2018-11-06 00:31:11 -0700

    Initial crate work, drawable done

---
+++ src/Crate.cpp
@@ -0,0 +1,53 @@
+/***************************************************************************//**
+* @file Crate.cpp
+* @author Jon McKee
+* @brief Contains all maintenance functions for Crate class.
+* *****************************************************************************/
+
+#include "Drawable.h"
+
+/***************************************************************************//**
+* @author Jon McKee
+* @brief Constructor
+*
+* Initiates the coordinates of where crate is to be drawn and from whom the
+* crate came from.
+*
+* @param[in] ID - indicates who fired the crate
+* @param[in] x_coor - coordinate to spawn crate
+* @param[in] y_coor - coordinate to spawn crate
+* *****************************************************************************/
+Crate::Crate( int ID, GLfloat x_coor, GLfloat y_coor, direction dir )
+{
+    screen_x = x_coor;
+    screen_y = y_coor;
+    tex = 4;
+}
+
+
+/***************************************************************************//**
+* @author Jon McKee
+* @brief draw
+*
+* Draws the crate in the gamefield
+* *****************************************************************************/
+void Crate::draw(int x, int y)
+{
+    glEnable(GL_TEXTURE_2D);
+    glPushMatrix();
+    glTranslatef(screen_x, screen_y, -5.0f);
+    glRotatef(angle,0,0,1);
+    glBindTexture(GL_TEXTURE_2D, sfxTex[tex]);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-0.07f * scalar, -0.07f * scalar,  1.0f);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f( 0.07f * scalar, -0.07f * scalar,  1.0f);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f( 0.07f * scalar,  0.07f * scalar,  1.0f);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(-0.07f * scalar,  0.07f * scalar,  1.0f);
+    glEnd();
+    glPopMatrix();
+    glDisable(GL_TEXTURE_2D);
+}
