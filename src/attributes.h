92891da2e090645266867fd9a85ccaf6cabccb8a
jamckee <dj32mckee@hotmail.com>
2019-02-13 09:42:22 -0700

    Removed shots from attributes

--- src/attributes.h
+++ src/attributes.h
@@ -14,12 +14,11 @@ struct attributes
     int tankHealth = 0;
     int tankDamage = 0;
     int tankAP = 0;
-    int tankShots = 0;
     int tankRadar = 0;
     int tankAmmo = 0;
     int projRange = 0;
-    attributes( int tH = 0, int tD = 0, int tR = 0, int tS = 0, int tV = 0, int tA = 0, int pR = 0) 
-       : tankHealth(tH), tankDamage(tD), tankAP(tR), tankShots(tS), tankRadar(tV), tankAmmo(tA), projRange(pR) {}
+    attributes( int tH = 0, int tD = 0, int tR = 0, int tV = 0, int tA = 0, int pR = 0) 
+       : tankHealth(tH), tankDamage(tD), tankAP(tR), tankRadar(tV), tankAmmo(tA), projRange(pR) {}
 
 };
 
